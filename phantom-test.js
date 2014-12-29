5909fe19974914f10f1a9e9cd2d2f831e16d96a8
Mark Hahn <mark@hahnca.com>
2014-12-28 23:14:18 -0800

    testing git control

--- phantom-test.js
+++
@@ -1,5 +0,0 @@
-var page = require('webpage').create();
-page.open('http://www.wunderground.com/history/airport/KLGB/2014/11/23/MonthlyHistory.html#calendar', function() {
-  page.render('phantom-test.png');
-  phantom.exit();
-});
